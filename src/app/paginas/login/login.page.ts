4f7114e3c3a401afb3e9cfc76bb8f3b0be28870a
Nicolas.Sande <nialsande@gmail.com>
2020-10-23 16:14:30 -0300

    login funcional

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  mail: string;
+  pass: string;
+  msjError: string;
+  logeando=true;
+  ocultarVerificar: boolean;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, public router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      mail: ['', Validators.required],
+      clave: ['', Validators.required]
+    });
   }
 
+  Entrar(){
+    const { mail, clave } = this.form.value;
+
+    this.authService.login(mail, clave).then( res => {
+      this.router.navigate(['/home']);
+    }).catch(err => this.presentAlert(err));
+
+    
+  }
+
+  Registrar() {
+    const { mail, clave } = this.form.value;
+
+    this.authService.register(mail, clave).then(res => {
+      this.router.navigate(['/login'])
+    }).catch(err => this.presentAlert(err));
+  }
+
+  Limpiar(){
+    this.form.setValue({
+      mail: "",
+      clave: ""
+    });
+  }
+
+  async presentAlert(errores) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Error',
+      message: errores,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  Invitado() {
+    this.form.setValue({
+      mail: "admin@mail.com",
+      clave: "adminmail"
+    });
+  }
+
+  AutoLog(usuario) {
+    switch (usuario) {
+      case "admin" :
+        this.form.setValue({
+          mail: "admin@admin.com",
+          clave: "111111"
+        });
+        break;
+      case "empleado" :
+        this.form.setValue({
+          mail: "empleado@empleado.com",
+          clave: "222222"
+        });
+        break;
+      case "cliente" :
+        this.form.setValue({
+          mail: "cliente@cliente.com",
+          clave: "333333"
+        });
+        break;      
+    }
+  }
 }
--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  register(email: string, password: string) {
+    return new Promise ((resolve, rejects) => { 
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => rejects(err));
+    });
+  }
+
+  login(email: string, password: string){
+    return new Promise ((resolve, rejects) => {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
+        resolve(user);
+      }).catch(err => rejects(err));
+    });
+  }
+
+  public logOut(){
+    return new Promise ((resolve, rejects) => {
+      firebase.auth().signOut().then(user => {
+        resolve(user);
+        this.router.navigate(['/login']);
+      }).catch(err => rejects(err));
+    });
+  }
+
+  getCurrentUserId(): string {
+    return firebase.auth().currentUser ? firebase.auth().currentUser.uid : null;
+  }
+
+  getCurrentUserMail(): string {
+    return firebase.auth().currentUser.email;
+  }
 }
