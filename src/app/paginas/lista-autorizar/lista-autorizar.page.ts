a72cf5d5bb0f92e23b6257bca302351b96b73e45
Nicolas.Sande <nialsande@gmail.com>
2020-11-26 00:07:20 -0300

    servicio envio de mail conectado

--- src/app/paginas/lista-autorizar/lista-autorizar.page.ts
+++ src/app/paginas/lista-autorizar/lista-autorizar.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { DataService } from 'src/app/servicios/data.service';
+import { MailService } from 'src/app/servicios/mail.service';
 
 @Component({
   selector: 'app-lista-autorizar',
@@ -12,7 +13,10 @@ export class ListaAutorizarPage implements OnInit {
 
   lista = [];
   
-  constructor(private data:DataService,private auth:AuthService,private toas:ToastrService) { }
+  constructor(private data:DataService,
+              private auth:AuthService,
+              private toas:ToastrService,
+              private mailService: MailService) { }
 
   ngOnInit() {
     
@@ -24,21 +28,21 @@ export class ListaAutorizarPage implements OnInit {
 
   }
 
-  autorizar(user:any)
-  {
+  autorizar(user:any) {
     this.auth.updateEstadoUsuario(user.uid,1).then(res =>{
-         this.toas.success("Se ha Aceptado con éxito al cliente");
-         
-         this.auth.registrar("dYMXr1MLTQetBD39hSUR4B:APA91bFCFeJ2TkMGtfhvd2rZDuLqJaip2TEylJHCw_tXVzFkKnwyhvZ-X6ztBXINBjSZMS0N64Sd0L80FPJe3zu-45cuSV7rUn-hqHxtqIp3TNmfMqGTrbBJxrjmm3qFAqg2kFlHr61i","Aceptado","Bienvenido a nuestro Restaurate","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkOtM0mYv0ZXxL5CmRThkJf5zkIbao3E3CgQ&usqp=CAU").toPromise().then(res =>{
-          console.info(res);
+        this.toas.success("Se ha Aceptado con éxito al cliente");
+        this.mailService.sendEmailAceptado(user.mail, user.nombre);
+        this.auth.registrar("dYMXr1MLTQetBD39hSUR4B:APA91bFCFeJ2TkMGtfhvd2rZDuLqJaip2TEylJHCw_tXVzFkKnwyhvZ-X6ztBXINBjSZMS0N64Sd0L80FPJe3zu-45cuSV7rUn-hqHxtqIp3TNmfMqGTrbBJxrjmm3qFAqg2kFlHr61i","Aceptado","Bienvenido a nuestro Restaurate","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkOtM0mYv0ZXxL5CmRThkJf5zkIbao3E3CgQ&usqp=CAU").toPromise().then(res =>{
+        console.info(res);
         })
          
     });
   }
-  rechazar(user:any)
-  {
+  
+  rechazar(user:any) {
     this.auth.updateEstadoUsuario(user.uid,-1).then(res =>{
       this.toas.success("Se ha rechazado con éxito al Cliente");
+      this.mailService.sendEmailRechazado(user.mail, user.nombre);
       this.auth.registrar("dYMXr1MLTQetBD39hSUR4B:APA91bFCFeJ2TkMGtfhvd2rZDuLqJaip2TEylJHCw_tXVzFkKnwyhvZ-X6ztBXINBjSZMS0N64Sd0L80FPJe3zu-45cuSV7rUn-hqHxtqIp3TNmfMqGTrbBJxrjmm3qFAqg2kFlHr61i","Rechazado","Lamentamos comunicarle que no fue aceptado para el ingreso al Restaurante","https://img.freepik.com/foto-gratis/hombre-triste-sosteniendo-cabeza-mano_1150-6346.jpg?size=626&ext=jpg").toPromise().then(res =>{
           console.info(res);
         })
